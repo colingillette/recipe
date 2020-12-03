61d42c045504ff16ec952e43c91fd990d8b65cef
Colin Gillette <colingillette35@gmail.com>
2020-12-02 18:51:35 -0600

    initial commit for develop

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>recipe</title>
+    <meta name="author" content="SitePoint">
+</head>
+
+<body>
+    <h1>recipe</h1>
+    <p>recipe app</p>
+</body>
+</html>
