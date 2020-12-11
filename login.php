c1363da644bb7fe4412ca478522e3a3b5393b743
Colin Gillette <colingillette35@gmail.com>
2020-12-11 16:41:03 -0600

    recipe-21: add login functionality

--- login.php
+++ login.php
@@ -6,11 +6,37 @@
 <head>
     <meta charset="utf-8">
     <title>recipe - login</title>
-    <meta name="author" content="SitePoint">
     <?php include_once("resources/cdn.php"); ?>
 </head>
 
 <body>
-    <h1>login</h1>
+    <div class="d-flex justify-content-center login-row">
+        <h1>login</h1>
+    </div>
+    <div class="d-flex justify-content-center login-row">
+        <?php
+            if (isset($_SESSION["loginError"])) {
+                if ($_SESSION["loginError"] == true) {
+                    echo '<p>That username was incorrect. Please Try again.</p>';
+                }
+            }
+        ?>
+    </div>
+    <div class="d-flex justify-content-center login-row">
+        <form action="server/scripts/login.php" method="post" name="loginForm">
+            <input type="text" placeholder="username" name="username"></input>
+    </div>
+
+    <div class="d-flex justify-content-center login-row">
+            <input type="submit" value="submit"></input>
+        </form>
+    </div>
+
+    <style>
+        .login-row {
+            margin-top: 2em;
+            margin-bottom: 1em;
+        }
+    </style>
 </body>
 </html>
--- server/scripts/login.php
+++ server/scripts/login.php
@@ -1,6 +1,65 @@
 <?php
 
+require_once("domain/user.php");
+
 session_start();
-session_destroy();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = test_input($_POST["username"]);
+}
+
+$conn = new mysqli("localhost","develop","develop","recipe");
+if ($conn->connect_error) {
+    exit();
+}
+
+$query = $conn->prepare("select id, username, displayName, joinDate, lastLogin, role from users where username = ?");
+$query->bind_param("s", $username);
+$query->execute();
+$result = $query->get_result();
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $user = new User();
+        
+        $user->setId($row["id"]);
+        $user->setUsername($row["username"]);
+        $user->setDisplayName($row["displayName"]);
+        $user->setJoinDate($row["joinDate"]);
+        $user->setLastLogin($row["lastLogin"]);
+        $user->setRole($row["role"]);
+        $user->setIsLoggedIn(true);
+
+        $_SESSION["user"] = $user;
+        logNewLogin($user, $conn);
+        header('Location: /recipe/index.php');
+    }
+    
+    $_SESSION["loginError"] = false;
+} else {
+    $_SESSION["loginError"] = true;
+    header('Location: /recipe/login.php');
+}
+
+$query->close();
+$conn->close();
+
+//------------------------------
+// Functions
+//------------------------------
+
+function logNewLogin($user, $conn) {
+    $lastLoginQuery = $conn->prepare("update users set lastLogin = now() where id = ?");
+    $lastLoginQuery->bind_param("i", $user->getId);
+    $lastLoginQuery->execute();
+    $lastLoginQuery->close();
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 
 ?>
